0e833aff09083e9076fd9c668781203e03fd5307
Sohan-Sunkari <88518634+Sohan-Sunkari@users.noreply.github.com>
2023-09-15 18:54:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/W4TFZ5cj/R.png');
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y;
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initiazlied');
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+    fill(255, 0, 0);
+    stroke(255,0,0);
+    image(clown_nose, noseX, noseY, 30, 20);
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
